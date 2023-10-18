970aca91576eeb5125b1bb5f78232530af20a374
Associate <Associate@roifmrwindt>
2023-10-18 09:35:58 +0530

    refactored sql

--- investmonkeyServer/sql/investMonkey.sql
+++ investmonkeyServer/sql/investMonkey.sql
@@ -31,8 +31,10 @@ CREATE TABLE client_identification(
 	IdentificationValue VARCHAR(20),
 	FOREIGN KEY (ClientId) REFERENCES client(ClientId)
 );
-INSERT INTO client VALUES('123abc','Jitin','Dodeja','123456789','jd@gmail.com',TO_DATE('1989-12-09','YYYY-MM-DD'),'India','570011','jd123');INSERT INTO client_identification VALUES('123abc','Aadhar','1234567');
-INSERT INTO client VALUES('456abc','Vishakha','V','6361724765','vishakhav965@gmail.com',TO_DATE('2000-10-09','YYYY-MM-DD'),'India','573201','vis123');INSERT INTO client_identification VALUES('456abc','Aadhar','872357934426');
+INSERT INTO client VALUES('123abc','Jitin','Dodeja','123456789','jd@gmail.com',TO_DATE('1989-12-09','YYYY-MM-DD'),'India','570011','jd123');
+INSERT INTO client_identification VALUES('123abc','Aadhar','1234567');
+INSERT INTO client VALUES('456abc','Vishakha','V','6361724765','vishakhav965@gmail.com',TO_DATE('2000-10-09','YYYY-MM-DD'),'India','573201','vis123');
+INSERT INTO client_identification VALUES('456abc','Aadhar','872357934426');
 
  
 
@@ -55,8 +57,7 @@ INSERT INTO balance (clientId, balance) VALUES ('456abc', 1000000);
     FOREIGN KEY (ClientId) REFERENCES client(ClientId)
    );
 
-   INSERT INTO CLIENT_PREF (ClientId, INVESTMENT_PURPOSE, INCOME_CATEGORY, RISK_TOLERANCE, LENGTH_OF_INVESTMENT)
-VALUES ('456abc', 'Retirement', 'CATEGORY_1_10000', 'LOW', 10);
+   INSERT INTO CLIENT_PREF (ClientId, INVESTMENT_PURPOSE, INCOME_CATEGORY, RISK_TOLERANCE, LENGTH_OF_INVESTMENT) VALUES ('456abc', 'Retirement', 'CATEGORY_1_10000', 'LOW', 10);
 
 CREATE TABLE invest_order (
     OrderId VARCHAR(255) PRIMARY KEY,
@@ -87,22 +88,19 @@ CREATE TABLE trade (
 
  
 
-INSERT INTO invest_order (OrderId, InstrumentId, Quantity, TargetPrice, Direction, ClientId)
-VALUES ('order1', 'instrument1', 100, 150, 'Buy', '456abc');
+INSERT INTO invest_order (OrderId, InstrumentId, Quantity, TargetPrice, Direction, ClientId) VALUES ('order1', 'instrument1', 100, 150, 'Buy', '456abc');
 
  
 
  
 
-INSERT INTO invest_order (OrderId, InstrumentId, Quantity, TargetPrice, Direction, ClientId)
-VALUES ('order2', 'instrument2', 200, 200, 'Sell', '456abc');
+INSERT INTO invest_order (OrderId, InstrumentId, Quantity, TargetPrice, Direction, ClientId) VALUES ('order2', 'instrument2', 200, 200, 'Sell', '456abc');
 
  
 
  
 
-INSERT INTO invest_order (OrderId, InstrumentId, Quantity, TargetPrice, Direction, ClientId)
-VALUES ('order3', 'instrument3', 150, 180, 'Buy', '456abc');
+INSERT INTO invest_order (OrderId, InstrumentId, Quantity, TargetPrice, Direction, ClientId) VALUES ('order3', 'instrument3', 150, 180, 'Buy', '456abc');
 
  
 
@@ -114,22 +112,19 @@ VALUES ('order3', 'instrument3', 150, 180, 'Buy', '456abc');
 
  
 
-INSERT INTO trade (TradeId, Quantity, ExecutionPrice, Direction, OrderId, CashValue, ClientId, InstrumentId)
-VALUES ('trade1', 100, 155, 'Buy', 'order1', 15500, '456abc', 'instrument1');
+INSERT INTO trade (TradeId, Quantity, ExecutionPrice, Direction, OrderId, CashValue, ClientId, InstrumentId) VALUES ('trade1', 100, 155, 'Buy', 'order1', 15500, '456abc', 'instrument1');
 
  
 
  
 
-INSERT INTO trade (TradeId, Quantity, ExecutionPrice, Direction, OrderId, CashValue, ClientId, InstrumentId)
-VALUES ('trade2', 200, 198, 'Sell', 'order2', 39600, '456abc', 'instrument2');
+INSERT INTO trade (TradeId, Quantity, ExecutionPrice, Direction, OrderId, CashValue, ClientId, InstrumentId) VALUES ('trade2', 200, 198, 'Sell', 'order2', 39600, '456abc', 'instrument2');
 
  
 
  
 
-INSERT INTO trade (TradeId, Quantity, ExecutionPrice, Direction, OrderId, CashValue, ClientId, InstrumentId)
-VALUES ('trade3', 150, 185, 'Buy', 'order3', 27750, '456abc', 'instrument3');
+INSERT INTO trade (TradeId, Quantity, ExecutionPrice, Direction, OrderId, CashValue, ClientId, InstrumentId) VALUES ('trade3', 150, 185, 'Buy', 'order3', 27750, '456abc', 'instrument3');
 
  
 
@@ -156,18 +151,15 @@ CREATE TABLE holding (
 
  
 
-INSERT INTO holding (InstrumentId, InstrumentDescription, CategoryId, AskNumber, Quantity, TotalPrice, ClientId)
-VALUES ('instrument1', 'Description 1', 'category1', 200, 100, 15000, '456abc');
+INSERT INTO holding (InstrumentId, InstrumentDescription, CategoryId, AskNumber, Quantity, TotalPrice, ClientId) VALUES ('instrument1', 'Description 1', 'category1', 200, 100, 15000, '456abc');
 
  
 
-INSERT INTO holding (InstrumentId, InstrumentDescription, CategoryId, AskNumber, Quantity, TotalPrice, ClientId)
-VALUES ('instrument2', 'Description 2', 'category2', 300, 150, 22500, '456abc');
+INSERT INTO holding (InstrumentId, InstrumentDescription, CategoryId, AskNumber, Quantity, TotalPrice, ClientId) VALUES ('instrument2', 'Description 2', 'category2', 300, 150, 22500, '456abc');
 
  
 
-INSERT INTO holding (InstrumentId, InstrumentDescription, CategoryId, AskNumber, Quantity, TotalPrice, ClientId)
-VALUES ('instrument3', 'Description 3', 'category3', 250, 120, 18000, '456abc');
+INSERT INTO holding (InstrumentId, InstrumentDescription, CategoryId, AskNumber, Quantity, TotalPrice, ClientId) VALUES ('instrument3', 'Description 3', 'category3', 250, 120, 18000, '456abc');
 
  
 
